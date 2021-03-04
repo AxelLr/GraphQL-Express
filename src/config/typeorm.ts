bea800a22e8c7312db864d3dab208f2029c5f570
Axel León <axelleonr12@gmail.com>
2021-03-04 14:42:47 -0300

    added redis and minor fixes

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -3,11 +3,11 @@ import {ConnectionOptions} from 'typeorm'
 import {__DEV__} from './constants'
 
 export const TYPEORM_CONFIG: ConnectionOptions = {
-  host: 'ec2-54-157-66-140.compute-1.amazonaws.com',
-  database: 'd125hvfv98ss57',
-  username: 'xziorgaccuwpoa',
-  port: 5432,
-  password: 'd0395f6e0de6ac23fe33a575be06351eee8d13b62203028caf235de61e547a6c',
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE,
+  username: process.env.DATABASE_USERNAME,
+  port:  +process.env.DATABASE_PORT!,
+  password:  process.env.DATABASE_PASSWORD,
   // username: process.env.DATABASE_USER,
   // password: process.env.DATABASE_PASSWORD,
   ssl: {
